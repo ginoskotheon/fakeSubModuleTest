15045bb2c83715791f1036148119d5d164d5ca78
William McDonald <mcdonaldw@comaco.com>
2019-04-04 12:55:26 -0500

    another change

--- addTitle.js
+++ addTitle.js
@@ -1,3 +1,7 @@
-function addText(text, id){
-    document.getElementById(id).innerHTML = text + " Have a great day!";
+function addText(text, id, choice){
+    if(choice === 1)
+        document.getElementById(id).innerHTML = text + " Have a great day!";
+    else 
+        document.getElementById(id).innerHTML = text + " ... or not?!";
+
 }
